98a2311dfaa6664c7a0a9aa13a60834931476922
Reshmacheck <reshmacheck93600@gmail.com>
2023-01-03 15:05:12 +0100

    category

--- fakestore/src/App.jsx
+++ fakestore/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import ProductDetail from "./pages/productDetail/ProductDetail";
 import "./App.css";
+import Category from "./pages/category/Category";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/details/:id" element={<ProductDetail />} />
+        <Route path="/category" element={<Category />} />
       </Routes>
     </div>
   );
---
+++ fakestore/src/pages/category/Category.jsx
@@ -0,0 +1,79 @@
+import "./category.css"
+
+const Category = () => {
+    return (
+    <>
+    <header>
+        <nav>
+        <div className="one">
+        <a href=""><i class="fa-solid fa-bars"/></a>
+        <a href="">My store</a>
+        </div>
+        <div className="two">
+        <a href="">3</a>
+        <a href=""><i class="fa-solid fa-cart-shopping"/></a>
+        </div>
+        </nav>
+    </header>
+
+    <section>
+        <h1>Electronic</h1>
+        <article>
+            <div>
+            <a href=""><img src="/html/placeholder.png" alt=""/></a>
+            <p>Product1<br/>brand
+                
+            </p>
+        </div>
+            <p>100$</p>
+
+            
+
+        </article>
+        <article>
+            <div>
+                <a href=""><img src="/html/placeholder.png" alt=""/></a>
+                <p>Product1<br/>brand
+                    
+                </p>
+            </div>
+            <p>100$</p>
+            
+        </article>
+        <article>
+            <div>
+                <a href=""><img src="/html/placeholder.png" alt=""/></a>
+                <p>Product1<br/>brand
+                    
+                </p>
+            </div>
+            <p>100$</p>
+            
+        </article>
+        <article>
+            <div>
+                <a href=""><img src="/html/placeholder.png" alt=""/></a>
+                <p>Product1<br/>brand
+                    
+                </p>
+            </div>
+            <p>100$</p>
+            
+        </article>
+        <article>
+            <div>
+                <a href=""><img src="/html/placeholder.png" alt=""/></a>
+                <p>Product1<br/>brand
+                    
+                </p>
+            </div>
+            <p>100$</p>
+            
+        </article>
+    </section>
+    </>
+    );
+
+}
+
+export default Category;
