010c12017b50798dadfc06e7e135daed93a6b6dd
doflamingo94 <khaledkhaled94400@hotmail.fr>
2023-01-03 15:14:19 +0100

    functional home

--- fakestore/src/pages/home/Home.jsx
+++ fakestore/src/pages/home/Home.jsx
@@ -1,19 +1,49 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
 
 const Home = () => {
-    return(
+    
+    const [data, setData] = useState([]); 
+
+    let fetchProducts = async () => {
+        let myRequest = new Request(
+            `https://fakestoreapi.com/products`,
+            {
+                method: "GET",
+            }
+        );
+
+        let request = await fetch(myRequest);
+        let data = await request.json();
+        // return data;
+        display(data);
+    };
+
+    let display = (data) => {
+        setData(data);
+        console.log(data);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+    
+    
+    return (
         <>
             
             <div className="main">
-                <div className="patternElement">
-                    <div className="leftMain">
-                        <img className="taille" src="/html/pictures/testeIMG.jpg" alt=""/>
-                            <p>Product 1 <br/> <span>Brand</span> </p>     
-                    </div>
-                    <div className="rightMain">
-                        <h4>100E</h4>
-                    </div>           
-                </div>       
+                {
+                    data.map((value, index) => <div className="patternElement" key={index}>
+                        <div className="leftMain">
+                            <img className="taille" src={value.image} alt=""/>
+                                <p>{value.title}<br/> <span>Brand</span> </p>     
+                        </div>
+                        <div className="rightMain">
+                            <h4>{value.price}€</h4>
+                        </div>           
+                    </div>)
+                }
             </div>
         </>
     )
